5aa70b111b53883254ffd8ae9d672261de6ce721
cactusboi <36212691+cactusboi@users.noreply.github.com>
2018-05-01 17:47:28 -0400

    oops i worked in a different file
    here it is

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public GameObject ProjectilePrefab;
+    public GameObject gameOverPanel;
+    public Text gameOverText;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        gameOverPanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            StartCoroutine(SpawnProjectile());
+        }
+    }
+
+    IEnumerator SpawnProjectile()
+    {
+
+
+        Instantiate(ProjectilePrefab, transform.position, Quaternion.identity);
+
+        int WaitTime = 0;
+        yield return new WaitForSeconds(WaitTime);
+        
+        if (Input.GetKeyDown("space"))
+        {
+            StartCoroutine(SpawnProjectile());
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Bird")
+        {
+
+            Destroy(gameObject);
+            Score.ScoreVal = 0;
+            gameOverPanel.SetActive(true);
+
+        }
+    }
+}
---
+++ Assets/SpawnObject.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObject : MonoBehaviour {
+
+    public GameObject BirdPrefab;
+    public GameObject Spawner;
+
+	// Use this for initialization
+	void Start () {
+
+        StartCoroutine(SpawnBird());
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	}
+
+    IEnumerator SpawnBird()
+    {
+        
+
+        Instantiate(BirdPrefab, transform.position, Quaternion.identity);
+
+        int WaitTime = Random.Range(0, 5);
+        yield return new WaitForSeconds(WaitTime);
+
+        StartCoroutine(SpawnBird());
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Destructible")
+        {
+            Destroy(gameObject);
+        }
+
+
+    }
+
+
+
+
+
+}
